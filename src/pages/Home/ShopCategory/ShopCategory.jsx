6747763cdf1bb73d258f3ef77691d3186f878ca0
masumbillahwebdeveloper <masumbillah.webdeveloper@gmail.com>
2023-05-19 19:36:56 +0600

    subcategory tab added

--- src/pages/Home/ShopCategory/ShopCategory.jsx
+++ src/pages/Home/ShopCategory/ShopCategory.jsx
@@ -1,17 +1,36 @@
 import { useEffect, useState } from "react";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import 'react-tabs/style/react-tabs.css';
 
 
 const ShopCategory = () => {
-    const [shop,setShop] =useState([]);
-    const [activeTab,setActiveTab] =useState('marvel');
-    useEffect(()=>{
+    const [shop, setShop] = useState([]);
+    const [activeTab, setActiveTab] = useState('marvel');
+    const [tabIndex, setTabIndex] = useState(0);
+    useEffect(() => {
         fetch(`http://localhost:5000/all-toy/${activeTab}`)
-        .then(res=>res.json())
-        .then(data=>setShop(data))
-    },[activeTab])
+            .then(res => res.json())
+            .then(data => setShop(data))
+    }, [activeTab])
     return (
         <div>
             <h2>Shop by Category</h2>
+            <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+                <TabList>
+                    <Tab>Title 1</Tab>
+                    <Tab>Title 2</Tab>
+                    <Tab>Title 2</Tab>
+                </TabList>
+                <TabPanel>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi et velit non libero quam dolor eligendi exercitationem debitis reprehenderit facere ipsa, ex sint! Dolore dicta eligendi magnam distinctio, aut a?</p>
+                </TabPanel>
+                <TabPanel>
+                    <p>masum</p>
+                </TabPanel>
+                <TabPanel>
+                    <p>Tinni</p>
+                </TabPanel>
+            </Tabs>
         </div>
     );
 };
--- src/pages/MyToy/MyToy.jsx
+++ src/pages/MyToy/MyToy.jsx
@@ -3,34 +3,79 @@ import { AuthContext } from "../../providers/AuthProvider";
 
 const MyToy = () => {
     const { user } = useContext(AuthContext)
-    const [toy, setToy] = useState([])
+    const [toys, setToys] = useState([])
     const [searchText, setSearchText] = useState("");
 
     useEffect(() => {
         fetch(`http://localhost:5000/my-toy/${user?.email}`)
             .then(res => res.json())
-            .then(data => setToy(data))
+            .then(data => setToys(data))
     }, [user])
 
     const handleSearch = () => {
         fetch(`http://localhost:5000/toy-name/${searchText}`)
-          .then((res) => res.json())
-          .then((data) => {
-            console.log(data);
-            setToy(data);
-          });
-      };
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setToys(data);
+            });
+    };
     return (
         <div>
-            <h2>My toy</h2>
-            <div>
+            <h2 className="text-5xl text-center mb-5">My toy</h2>
+            <div className="mb-5 text-center">
                 <input
                     onChange={(e) => setSearchText(e.target.value)}
                     type="text"
-                    className=""
+                    className="border-2 border-black mr-2 px-3"
                 />
-                <button onClick={handleSearch}>Search</button>
+                <button className="btn btn-info btn-sm" onClick={handleSearch}>Search</button>
+                
             </div>
+
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                        <th>SL</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Action Taken</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {toys?.map((toy, index) => (
+                        <tr key={toy._id}>
+
+                            <td>
+                            {index + 1}
+                            </td>
+                            
+                            <td>
+                                {toy.toyName}
+                            </td>
+                            <td>
+                            {toy.price}
+                            </td>
+                            <td>{toy.quantity}</td>
+                            <td>
+                                <button className="btn btn-outline btn-warning mr-2">Update</button>
+                                <button className="btn btn-outline btn-error">Delete</button>
+                            </td>
+
+                        </tr>
+                        ))}
+
+                    </tbody>
+
+
+                </table>
+            </div>
+
+
+
         </div>
     );
 };
