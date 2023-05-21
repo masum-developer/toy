250fec9584193013f034433825d3f21595cc5c6d
masumbillahwebdeveloper <masumbillah.webdeveloper@gmail.com>
2023-05-21 22:54:54 +0600

    slider new image added

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,7 +1,7 @@
 import imgHulk from '../../../assets/images/banner/hulk.jpg';
 import imgSpider from '../../../assets/images/banner/spider.jpg';
 import imgSlider3 from '../../../assets/images/banner/slider3.jpg';
-import imgAvenger from '../../../assets/images/banner/avenger.jpg';
+import imgTransformer from '../../../assets/images/banner/transformer.jpg';
 
 const Banner = () => {
     return (
@@ -29,7 +29,7 @@ const Banner = () => {
                 </div>
             </div>
             <div id="slide4" className="carousel-item relative w-full">
-                <img src={imgAvenger} className="w-full" />
+                <img src={imgTransformer} className="w-full" />
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide3" className="btn btn-circle">❮</a>
                     <a href="#slide1" className="btn btn-circle">❯</a>
