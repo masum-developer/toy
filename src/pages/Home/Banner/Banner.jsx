bebcaf3b5b1dde8bfdb1f3b1b48f60e8d6e616bc
masumbillahwebdeveloper <masumbillah.webdeveloper@gmail.com>
2023-05-21 22:21:39 +0600

    edit banner

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,7 +1,8 @@
 import imgHulk from '../../../assets/images/banner/hulk.jpg';
 import imgSpider from '../../../assets/images/banner/spider.jpg';
-import imgCaptain from '../../../assets/images/banner/captain.jpg';
+import imgSlider3 from '../../../assets/images/banner/slider3.jpg';
 import imgAvenger from '../../../assets/images/banner/avenger.jpg';
+
 const Banner = () => {
     return (
         <div className="carousel w-full">
@@ -21,7 +22,7 @@ const Banner = () => {
                 </div>
             </div>
             <div id="slide3" className="carousel-item relative w-full">
-            <img src={imgCaptain} className="w-full" />
+            <img src={imgSlider3} className="w-full" />
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide2" className="btn btn-circle">❮</a>
                     <a href="#slide4" className="btn btn-circle">❯</a>
